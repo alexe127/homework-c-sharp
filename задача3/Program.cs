275f0f8cefb4da95bc172b56f96f935dfad47dab
«Alex» <aser_e@mail.ru>
2022-03-05 23:19:04 +0900

    задача3

---
+++ задача3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine ("число " + num + " чётное");
+}
+else
+{
+    Console.WriteLine ("число " + num + " не чётное");
+}
