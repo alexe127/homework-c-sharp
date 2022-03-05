8f54a3a5c55d9be5e9b5b17d2967f22f10b456a2
«Alex» <aser_e@mail.ru>
2022-03-06 00:29:33 +0900

    задача7

---
+++ задача7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число");
+int num = new Random() .Next(100,1000);
+    Console.WriteLine(num);
+int a = num / 100;
+
+int b = num % 10;
+
+Console.Write(a);
+Console.Write(b);
+
