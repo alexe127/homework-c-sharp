8af9552f6504b0197449cb7d23fd42ada18849ba
«Alex» <aser_e@mail.ru>
2022-03-06 01:23:46 +0900

    задача9

---
+++ задача9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;        
+    }
+    num = num % 10;
+    Console.WriteLine(num); 
+}
+
