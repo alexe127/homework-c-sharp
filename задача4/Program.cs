122afdece53400b9371a2d04f342fe9cf5f7d751
«Alex» <aser_e@mail.ru>
2022-03-05 23:33:31 +0900

    задача4

---
+++ задача4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num < 1000)
+{
+    Console.WriteLine("последняя цифра= "+ num % 10);
+}
+else
+{
+    Console.WriteLine("введите трёхзначное число");
+}
