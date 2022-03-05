7d41b72c5441de4d40f4b0e233b265059a58e73b
«Alex» <aser_e@mail.ru>
2022-03-06 00:50:06 +0900

    задача8

---
+++ задача8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите число");
+double num1 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine ("число " + num1 + " кратно числу " + num2);
+}
+else
+{
+    Console.WriteLine ("число " + num1 + " не кратно числу " + num2);
+    Console.WriteLine ("остаток от деления " + num1 / num2);
+
+}
